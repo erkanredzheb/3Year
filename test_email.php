50da7e326f275de8e1c0dacf474403b0ac1d0acc
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-18 20:54:23 +0000

    verify e-mails

---
+++ test_email.php
@@ -0,0 +1,29 @@
+<?php
+
+require("connectDB.php");
+require("register.php");
+
+
+$dbname = "3year";
+mysqli_select_db($conn, $dbname);
+
+
+// Functional tests
+echo "<br>" . "This sould create a new entry:" . "<br>";
+verifyReg($username = "email", $password = "magik", $password2 = "magik", $mails = "magik@abv.bg");
+
+
+// Delete previously created entries with username "password".
+$testemail = "email";
+$stmt = $conn->prepare("DELETE FROM user_info WHERE username = ?");
+$stmt->bind_param("s", $testemail);
+$stmt->execute();
+
+
+// Unit tests
+echo "<br>" . "This sould say that the e-mail is not valid:" . "<br>";
+verifyReg($username = "email", $password = "magik", $password2 = "magik", $mails = "magik@abv");
+
+$conn->close();
+
+?>
