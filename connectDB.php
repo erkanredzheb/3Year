78a4965ee79f377a6eaaff92bc082f573d3d6da9
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-18 14:04:30 +0000

    Added a boolean to help testing

--- connectDB.php
+++ connectDB.php
@@ -8,9 +8,11 @@ $conn = new mysqli($servername, $usernameDB, $passwordDB);
 
 // Check connection
 if ($conn->connect_error) {
+	$verifyConn = false;
     die("Connection failed: " . $conn->connect_error);
 } 
-echo "Connected successfully";
+//echo "Connected successfully";
+$verifyConn = true;
 
 
 
