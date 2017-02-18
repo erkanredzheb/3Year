b1bd768ab41dee03e3b44c801afade8074ec450c
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-18 20:30:58 +0000

    updated var name

--- test_validusername.php
+++ test_validusername.php
@@ -9,12 +9,12 @@ mysqli_select_db($conn, $dbname);
 
 
 // Delete previously created entries with username "magik".
-$theuname = "magik";
+$testuname = "magik";
 $stmt = $conn->prepare("DELETE FROM user_info WHERE username = ?");
-$stmt->bind_param("s", $theuname);
+$stmt->bind_param("s", $testuname);
 $stmt->execute();
 
-
+// Functional tests
 echo "<br>" . "This sould create a new entry:" . "<br>";
 verifyReg($username = "magik", $password = "magik", $password2 = "magik", $mails = "magik@abv.bg");
 echo "<br>";
@@ -23,6 +23,8 @@ echo "Should say that the user already exists:" . "<br>";
 verifyReg($username = "magik", $password = "magik", $password2 = "magik", $mails = "magik@abv.bg");
 echo "<br>";
 
+
+// Unit tests
 echo "<br>" . "Should say that the username is too short:" . "<br>";
 verifyReg($username = "mag", $password = "magik", $password2 = "magik", $mails = "magik@abv.bg");
 echo "<br>";
