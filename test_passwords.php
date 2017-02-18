10ae85e15595d1f8cf12f6100a3be0776850341c
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-18 20:48:53 +0000

    for verifying passwords

---
+++ test_passwords.php
@@ -0,0 +1,39 @@
+<?php
+	
+require("connectDB.php");
+require("register.php");
+
+
+$dbname = "3year";
+mysqli_select_db($conn, $dbname);
+
+
+// Functional tests
+echo "<br>" . "This sould create a new entry:" . "<br>";
+verifyReg($username = "password", $password = "magik", $password2 = "magik", $mails = "magik@abv.bg");
+
+
+// Delete previously created entries with username "password".
+$testuname = "password";
+$stmt = $conn->prepare("DELETE FROM user_info WHERE username = ?");
+$stmt->bind_param("s", $testuname);
+$stmt->execute();
+
+
+// Unit tests
+echo "<br>" . "Should say that the password is too short:" . "<br>";
+verifyReg($username = "magik", $password = "magi", $password2 = "magi", $mails = "magik@abv.bg");
+echo "<br>";
+
+echo "<br>" . "Should say that the password is too long:" . "<br>";
+verifyReg($username = "magik", $password = "magikmagikmagikmagik", $password2 = "magikmagikmagikmagik", $mails = "magik@abv.bg");
+echo "<br>";
+
+echo "<br>" . "Should say that the password do not match:" . "<br>";
+verifyReg($username = "password", $password = "magik", $password2 = "magikk", $mails = "magik@abv.bg");
+echo "<br>";
+
+$conn->close();
+
+
+?>
