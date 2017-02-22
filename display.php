1cef27a197a59bd2a54e42edf48202fb50c8346d
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-22 23:08:27 +0000

    put display products in a new file

---
+++ display.php
@@ -0,0 +1,28 @@
+<?php
+
+displayimg();
+function displayimg()
+{
+	require("connectDB.php");
+
+    $dbname = "3year";
+    mysqli_select_db($conn, $dbname);
+
+    $stmt = $conn->prepare("SELECT * FROM product_info");
+    $stmt->execute();
+    $result = $stmt->get_result();
+    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+    {	
+    	echo $row['title'] . "<br>";
+    	echo $row['category'] . "<br>";
+    	echo $row['description'] . "<br>";
+    	echo $row['price'] . "<br>";
+    	echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
+    }
+
+
+    $stmt->close();
+    $conn->close();
+}
+
+?>
