9ec43442a2a9396ec2bfb02a5eac75d6cda7eaf1
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-22 22:14:15 +0000

    uploading a product sucessfully

--- upload.php
+++ upload.php
@@ -1,16 +1,45 @@
 <?php
 if(isset($_POST['submitimg']))
 {
-	$title = $_POST['title'];
-	$category = $_POST['state'];
-	$descr = $_POST['descr'];
-	$price = $_POST['price'];
+	if(getimagesize($_FILES['image']['tmp_name']) == FALSE)
+	{
+		echo "Please select an image.";
+	}
+	else
+	{	
+		$image = ($_FILES['image']['tmp_name']);
+		$image = file_get_contents($image);
+		$image = base64_encode($image);
+
+		$title = $_POST['title'];
+		$category = $_POST['state'];
+		$descr = $_POST['descr'];
+		$price = $_POST['price'];
+        saveimg($title, $category, $descr, $price, $image);
+		
+	}	
 
 	//echo $title . $category . $descr . $price;
 }	
 
+function saveimg($title, $category, $descr, $price, $image)
+{
+    	require("connectDB.php");
 
+        $dbname = "3year";
+        mysqli_select_db($conn, $dbname);
 
+        $stmt = $conn->prepare("INSERT INTO product_info (title, category, description, price, img) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssis", $title, $category, $descr, $price, $image);
+        $stmt->execute();
+
+    
+        echo "Product uploaded successfully." . "<br>";
+    
+
+        $stmt->close();
+        $conn->close();
+}
 
 ?>
 
