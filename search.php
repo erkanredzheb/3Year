366fd04247c4a1f22642000e0130592b550ce6bc
erkanredzheb <e_r_k_a_n@abv.bg>
2017-02-23 11:29:08 +0000

    search by exact title

---
+++ search.php
@@ -0,0 +1,42 @@
+<?php
+
+if(isset($_POST['searchButton']))
+{
+	$search = $_POST['search'];
+	
+    require("connectDB.php");
+
+    $dbname = "3year";
+    mysqli_select_db($conn, $dbname);
+    
+    $stmt = $conn->prepare("SELECT * FROM product_info WHERE title = ?");
+    $stmt->bind_param("s", $search);
+    $stmt->execute();
+    $result = $stmt->get_result();
+   
+    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+    {	
+    	echo $row['title'] . "<br>";
+    	echo $row['category'] . "<br>";
+    	echo $row['description'] . "<br>";
+    	echo $row['price'] . "<br>";
+    	echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
+    }
+     
+    $stmt->close();
+    $conn->close();
+
+}	
+
+?>
+
+<html>
+
+<form action="search.php" method="post">
+
+Search: <input type="text" name="search" value=""><br>
+
+<input type="submit" value="Go!" name="searchButton" />
+
+
+</html>
