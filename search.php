c00a7d1213b147eb2149b8853ca5bf338c88b51b
erkanredzheb <e_r_k_a_n@abv.bg>
2017-03-02 23:03:02 +0000

    search showing view buttons

--- search.php
+++ search.php
@@ -20,11 +20,28 @@ if(isset($_POST['searchButton']))
     	while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
     	{	
     		echo $row['title'] . "<br>";
-    		echo $row['category'] . "<br>";
-    		echo $row['description'] . "<br>";
-    		echo $row['price'] . "<br>";
-    		echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
-    	}
+            echo $row['category'] . "<br>";
+            echo $row['description'] . "<br>";
+            echo $row['price'] . "<br>";
+            echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
+        
+
+            echo "<html>";
+            echo "<head>";
+                echo "<link rel='stylesheet' type='text/css' href='./style.css'>";
+                echo "<script src='./scripts.js'></script>";
+                echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
+            echo "</head>";
+            echo "<body>";
+            
+            echo "<div class='product_view_button' onclick=\"product_view('".$row['id']."')\">View</div>";
+
+            echo "<br>";
+            echo "<br>";
+
+            echo "</body>";
+            echo "</html>";
+    	   }
     }
     else
     {
