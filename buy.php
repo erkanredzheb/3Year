d6d130fb1e4f41be0bfab3255e90c2800a345946
erkanredzheb <e_r_k_a_n@abv.bg>
2017-03-07 14:29:59 +0000

    products are bought - only acc balance changes yet

--- buy.php
+++ buy.php
@@ -1,9 +1,52 @@
 <?php
 
-// $id = $_POST['id'];
-
+session_start();
 $id = $_GET['id'];
 
-echo $id;
+require("connectDB.php");
+$dbname = "3year";
+mysqli_select_db($conn, $dbname);
+
+
+
+$stmt = $conn->prepare("SELECT price,user_id FROM product_info WHERE id = ?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+{	
+    	$price = $row['price'];
+    	$sellerID = $row['user_id'];
+    	
+}
+
+$userID = $_SESSION["user"];
+$stmt = $conn->prepare("SELECT acc_balance FROM user_info WHERE username = ?");
+$stmt->bind_param("s", $userID);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
+{	
+    	$accBal = $row['acc_balance'];
+    	
+}
+
+$newAccBalBuyer = $accBal - $price;
+$stmt = $conn->prepare("UPDATE user_info SET acc_balance = ? WHERE username = ?");
+$stmt->bind_param("is", $newAccBalBuyer, $userID);
+$stmt->execute();
+
+
+$newAccBalSeller = $accBal + $price;
+$stmt = $conn->prepare("UPDATE user_info SET acc_balance = ? WHERE username = ?");
+$stmt->bind_param("is", $newAccBalSeller, $sellerID);
+$stmt->execute();
+
+
+
+$stmt->close();
+$conn->close();
 
 ?>
--- index.php
+++ index.php
@@ -27,31 +27,34 @@ $stmt = $conn->prepare("SELECT * FROM product_info ORDER BY viewCounter DESC LIM
     $result = $stmt->get_result();
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
     {	
-    	echo $row['title'] . "<br>";
-    	echo $row['category'] . "<br>";
-    	echo $row['description'] . "<br>";
-    	echo $row['price'] . "<br>";
-    	echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
-        echo "By " . $row['user_id'] . "<br>";
+        if($row["boughtby"] == NULL)
+        {    
+    	   echo $row['title'] . "<br>";
+    	   echo $row['category'] . "<br>";
+    	   echo $row['description'] . "<br>";
+    	   echo $row['price'] . "<br>";
+    	   echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
+            echo "By " . $row['user_id'] . "<br>";
         
 
-        echo "<html>";
-        echo "<head>";
-            echo "<link rel='stylesheet' type='text/css' href='./style.css'>";
-            echo "<script src='./scripts.js'></script>";
-            echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
-        echo "</head>";
-        echo "<body>";
+            echo "<html>";
+            echo "<head>";
+                echo "<link rel='stylesheet' type='text/css' href='./style.css'>";
+                echo "<script src='./scripts.js'></script>";
+                echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
+            echo "</head>";
+            echo "<body>";
            
 
 
-        echo "<div class='product_view_button' onclick=\"product_view('".$row['id']."')\">View</div>";
+            echo "<div class='product_view_button' onclick=\"product_view('".$row['id']."')\">View</div>";
 
-        echo "<br>";
-        echo "<br>";
+            echo "<br>";
+            echo "<br>";
 
-        echo "</body>";
-        echo "</html>";
+            echo "</body>";
+            echo "</html>";
+        }
         
     }
 
@@ -67,31 +70,34 @@ $stmt = $conn->prepare("SELECT * FROM product_info ORDER BY viewCounter DESC LIM
     $result = $stmt->get_result();
     while($row = mysqli_fetch_array($result,MYSQLI_ASSOC))
     {	
-    	echo $row['title'] . "<br>";
-    	echo $row['category'] . "<br>";
-    	echo $row['description'] . "<br>";
-    	echo $row['price'] . "<br>";
-    	echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
-        echo "By " . $row['user_id'] . "<br>";
+        if($row['boughtby'] == NULL)
+        {    
+    	   echo $row['title'] . "<br>";
+    	   echo $row['category'] . "<br>";
+    	   echo $row['description'] . "<br>";
+    	   echo $row['price'] . "<br>";
+    	   echo '<img height="300" width="300" src="data:image;base64, '.$row['img'].' ">' . "<br>";
+            echo "By " . $row['user_id'] . "<br>";
         
 
-        echo "<html>";
-        echo "<head>";
-            echo "<link rel='stylesheet' type='text/css' href='./style.css'>";
-            echo "<script src='./scripts.js'></script>";
-            echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
-        echo "</head>";
-        echo "<body>";
+            echo "<html>";
+            echo "<head>";
+                echo "<link rel='stylesheet' type='text/css' href='./style.css'>";
+                echo "<script src='./scripts.js'></script>";
+                echo "<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>";
+            echo "</head>";
+            echo "<body>";
            
 
 
-        echo "<div class='product_view_button' onclick=\"product_view('".$row['id']."')\">View</div>";
+            echo "<div class='product_view_button' onclick=\"product_view('".$row['id']."')\">View</div>";
 
-        echo "<br>";
-        echo "<br>";
+            echo "<br>";
+            echo "<br>";
 
-        echo "</body>";
-        echo "</html>";
+            echo "</body>";
+            echo "</html>";
+        }    
         
     }
 
